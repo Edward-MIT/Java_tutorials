b6c7e48ba43b76815f9189df7dd4a3b9e16248ab
Edward_MIT <usmanoff19980404@gmail.com>
2026-04-24 22:53:03 +0900

    Arithmetic in Java

---
+++ Arithmetic.java
@@ -0,0 +1,30 @@
+public class Arithmetic {
+  public static void main (String[] args){
+    int peopleInRoom = 0;
+    peopleInRoom ++;
+    peopleInRoom ++;
+    peopleInRoom ++;
+    System.out.println(peopleInRoom);
+
+    peopleInRoom--;
+
+    System.out.println(peopleInRoom);
+
+    // ikkinchi misol
+    int x = 15;
+    int y = 4;
+
+    System.out.println("x + y = " + (x + y)) ;// 19
+    System.out.println("x - y = " + (x - y )); // 11
+    System.out.println("x * y = " + (x + y)) ; // 60
+
+    // butun sonni butun songa bo'lganda natija ham butun bo'ladi
+    System.out.println("x / y = " + (x / y)) ; // 3
+
+    // Qoldiqli bo'lish moduli
+    System.out.println("x % y = " + (x % y)) ; // 3 faqat qoldiq natijda chiqariladi
+
+
+  }
+
+}
