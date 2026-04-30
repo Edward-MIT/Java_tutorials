3ed9539e1de45ea349336c7772babde53a93e582
Edward_MIT <usmanoff19980404@gmail.com>
2026-04-30 22:24:03 +0900

    murkkabllashtirilgan so'zni palibronligini aniqlovchi dastur

--- Code_challange_1.java
+++ Code_challange_1.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Code_challange_1 {
   public static void main (String[] args){
+
+    // Kiyik so'zini palidrom ekanini aniqlovchi dastur
+
     String soz = "kiyik";
     String reversed = new StringBuilder(soz).reverse().toString();
 
@@ -8,5 +13,25 @@ public class Code_challange_1 {
     }else{
       System.out.println(soz + "- palidrom emas ");
     }
+
+    // foydalanuvchi kiritgan so'zni palidrom ekanini aniqlovchi dastur
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("So'zni kiriting: ");
+
+    String word = scanner.nextLine();
+
+    String reversed1 = new StringBuilder(word).reverse().toString();
+
+    if (word.equalsIgnoreCase(reversed1)){
+      System.out.println("'" + word + "'- palidrom");
+
+    }else{
+      System.out.println("'" + word + "' - palidrom emas ");
+    }
+
+
+    scanner.close();
+
   }
 }
